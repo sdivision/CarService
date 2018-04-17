b6e1b1791dd3a24b6c551a9f0888cb6dcb333ecb
sdivision <corrida17@gmail.com>
2018-04-18 02:47:28 +0300

    update email content

--- modules/core/src/com/company/carservice/jmx/EmployeeWorker.java
+++ modules/core/src/com/company/carservice/jmx/EmployeeWorker.java
@@ -1,8 +1,7 @@
 package com.company.carservice.jmx;
 
 import com.company.carservice.entity.Employee;
-import com.haulmont.cuba.core.Persistence;
-import com.haulmont.cuba.core.TypedQuery;
+import com.haulmont.cuba.core.*;
 import com.haulmont.cuba.core.app.EmailerAPI;
 import com.haulmont.cuba.core.app.EmailerConfig;
 import com.haulmont.cuba.core.global.Configuration;
@@ -11,6 +10,9 @@ import com.haulmont.cuba.security.app.Authenticated;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.List;
 
 @Component("carservice_EmployeeWorkerMBean")
@@ -55,14 +57,46 @@ public class EmployeeWorker implements EmployeeWorkerMBean {
             return typedQuery.getResultList();
         });
         for (Employee e : employees){
+
+
+
             try {
-                emailer.sendEmail(e.getEmail(), "Greetings", "Поздравляем вас с днём рождения, уважаемый "
-                        + e.getFirstName());
+                emailer.sendEmail(e.getEmail(), "Поздравляем с днём рождения!",
+                        "Поздравляем вас с днём рождения, уважаемый " + e.getFirstName() +
+                              "! \nЖелаем всего наилучшего в " + getAge(e) + "! \n" +
+                              "С уважением, коллектив автосервиса " + getEmployeeCenter(e) + ".");
             } catch (EmailException e1) {
                 e1.printStackTrace();
             }
         }
         return "Successful";
+    }
+
+    public String getEmployeeCenter(Employee employee){
+        String result;
+        try (Transaction tx = persistence.createTransaction()) {
+            EntityManager em = persistence.getEntityManager();
+            Query query = em.createQuery("select csc.name from carservice$CarServiceCenter csc " +
+                    "join carservice$Employee emp " +
+                    "where emp.center.id = csc.id and emp.id = :employee");
+            query.setParameter("employee",employee.getId());
+            result = String.valueOf(query.getFirstResult());
+            tx.commit();
+        }
+        return result;
+    }
+
+    public String getAge(Employee employee){
+        LocalDate date = employee.getBirthDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate now = LocalDate.now();
+        String age = String.valueOf(Period.between(date, now).getYears());
+        char lastDigit = age.charAt(age.length() - 1);
+        if (lastDigit == '1') {
+            return "ваш " + age + " год";
+        }
+        if (lastDigit == '2' || lastDigit == '3' || lastDigit == '4')
+            return "ваши " + age + " года";
+        return "ваши " + age + " лет";
     }
 
 }
